19ea6b75a7172fe19ce312b6d1a0845178192820
mangesh-takik-amla <mangesh.takik@amla.io>
2026-04-03 16:16:01 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,10 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// ✅ Add CORS policy
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
@@ -23,36 +23,18 @@ builder.Services.AddCors(options =>
     });
 });
 
-// ✅ Allow ALL origins
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll", policy =>
-    {
-        policy
-            .AllowAnyOrigin()
-            .AllowAnyHeader()
-            .AllowAnyMethod();
-    });
-});
-
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 builder.Services.AddSingleton<JsonFileService>();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-// ✅ Use CORS (IMPORTANT: before MapControllers)
-app.UseCors("AllowFrontend");
 
 app.UseHttpsRedirection();
 
